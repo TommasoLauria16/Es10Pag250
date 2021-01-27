c96515e9558f6364f013c63c28ffc6693385aff1
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2021-01-27 12:51:01 +0100

    Add project files.

---
+++ Es10/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Gestione;
+
+namespace Es10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool vf = false;
+            bool dS = false;
+            Console.WriteLine("vuoi effettuare il riempimento casuale o manuale");
+            string str = Console.ReadLine();
+            if (str == "casuale" || str == "Casuale" || str == "CASUALE")
+                vf = true;
+            ClasseArray.Riempimento(vf);
+            Console.WriteLine("vuoi stapare l'array da destra a sinista digita DS;\nse vuoi stamparlo da sinistra a destra SD");
+            string destra_Sinistra = Console.ReadLine();
+            if (destra_Sinistra == "DS" || destra_Sinistra == "Ds" || destra_Sinistra == "ds")
+                dS = true;
+            ClasseArray.OrdineStampa(dS);
+            double[] array = ClasseArray.Ricerca();
+            Console.WriteLine($"\nil massimo è {array[0]};\nil minimo è {array[1]};\nla media è {array[2]}.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Gestione/ClasseArray.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Gestione
+{
+    public class ClasseArray
+    {
+        private const int a = 5;
+        private static int[] array = new int[a];
+        public static void Riempimento(bool vf)
+        {
+            Random r = new Random();
+            if (vf == true)
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    array[i] = r.Next(1000);
+                }
+            }
+            else
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    Console.WriteLine($"inserisci il valore in posizione {i}");
+                    array[i] = int.Parse(Console.ReadLine());
+                }
+            }
+        }
+        public static void OrdineStampa(bool destraSinistra)
+        {
+            if (destraSinistra == true)
+            {
+                for (int i = array.Length - 1; i >= 0; i--)
+                {
+                    Console.Write($"{array[i]} ");
+                }
+            }
+            else
+            {
+                for (int i = 0; i < array.Length;i++)
+                {
+                    Console.Write($"{array[i]} ");
+                }
+            }
+
+        }
+        public static double[] Ricerca()
+        {
+
+            int max = int.MinValue;
+            int min = int.MaxValue;
+            double somma = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                somma += array[i];
+                if (array[i] > max)
+                    max = array[i];
+                if (array[i] < min)
+                    min = array[i];
+                //min = Math.Min(min, array[i]);
+                //max = Math.Max(max, array[i]);
+            }
+            double media = somma / array.Length;
+            //double[] maxMinMedia = new double[] { max, min, media };
+            //return maxMinMedia;
+            return new double[] { max, min, media };
+
+
+
+        }
+    }
+}
